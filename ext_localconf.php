ee03db9e6bc86ceec6332df686b54646f77ea686
nanna-dk <nanna@e-nanna.dk>
2022-11-25 15:21:06 +0100

    Added icons

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,7 +11,7 @@ defined('TYPO3') or die('Access denied.');
 (function ($extKey='ku_course_doktype') {
     $courseDoktype = 116;
 
-    // Provide icon for page tree, list view, ... :
+    // Provide icon for page tree, list view, etc. See Configuration/TCA/Overrides/pages.php
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
     $iconRegistry
         ->registerIcon(
@@ -29,6 +29,22 @@ defined('TYPO3') or die('Access denied.');
                 'source' => 'EXT:' . $extKey . '/Resources/Public/Icons/Extension.svg',
             ]
         );
+    $iconRegistry
+    ->registerIcon(
+        'ku-course-doktype-icon-root',
+        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        [
+            'source' => 'EXT:' . $extKey . '/Resources/Public/Icons/Extension.svg',
+        ]
+    );
+    $iconRegistry
+    ->registerIcon(
+        'ku-course-doktype-icon-hideinmenu',
+        TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        [
+            'source' => 'EXT:' . $extKey . '/Resources/Public/Icons/Extension.svg',
+        ]
+    );
 
     // Allow backend users to drag and drop the new page type:
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
